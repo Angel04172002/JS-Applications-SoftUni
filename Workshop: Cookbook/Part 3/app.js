7a91c73ed4f3119f569cd08857442b02c7584a38
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-08-04 23:17:45 +0300

    Create app.js

---
+++ Workshop: Cookbook/Part 3/app.js
@@ -0,0 +1,24 @@
+import { router } from "./router.js";
+import { updateAuth } from "./auth.js";
+
+updateAuth();
+router('/');
+
+const mainNavigationElement = document.querySelector('.navigation');
+
+mainNavigationElement.addEventListener('click', (e) => {
+
+    e.preventDefault();
+
+    if(e.target.tagName === 'A') {
+
+        let url = new URL(e.target.href);
+
+        document.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
+
+        router(url.pathname);
+    };
+});
+
+
