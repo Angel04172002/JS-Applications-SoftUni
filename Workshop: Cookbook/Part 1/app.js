63fe32114cdab20fb377fa82dda9db36186ef228
Angel04172002 <58862906+Angel04172002@users.noreply.github.com>
2023-09-14 11:41:57 +0300

    Update app.js

--- Workshop: Cookbook/Part 1/app.js
+++ Workshop: Cookbook/Part 1/app.js
@@ -1,85 +1,88 @@
-const baseUrl = 'http://localhost:3030';
+const url = 'http://localhost:3030/jsonstore/cookbook/recipes';
+const detailsUrl = 'http://localhost:3030/jsonstore/cookbook/details';
 
 
+window.onload = function () {
 
-window.addEventListener('load', () => {
-
-    fetch(`${baseUrl}/jsonstore/cookbook/recipes`)
+    fetch(url)
         .then(res => res.json())
-        .then(recipes => {
-            renderRecipes(Object.values(recipes));
+        .then(data => {
+            renderRecipes(Object.values(data));
         })
         .catch(err => console.log(err));
-});
+};
 
 
 function renderRecipes(recipes) {
 
     const mainElement = document.querySelector('main');
+
     mainElement.innerHTML = '';
 
-    for (let item of recipes) {
-        const recipe = createRecipe(item);
-        mainElement.appendChild(recipe);
-    };
+    recipes.forEach(recipe => {
+        mainElement.appendChild(createRecipe(recipe));
+    });
+
+    mainElement.addEventListener('click', showMoreInfo);
 }
+
 
 function createRecipe(recipe) {
 
-    const previewArticle = article(
-        { class: 'preview' },
+    return article(
+        { class: 'preview', _id: recipe._id },
         div(
             { class: 'title' },
-            createElement('h2', {}, recipe.name)
+            heading(2, {}, `${recipe.name}`)
         ),
         div(
             { class: 'small' },
-            img(
-                { src: recipe.img }
-            ),
+            img({ src: `${recipe.img}` })
         ),
     );
+}
 
-    previewArticle.addEventListener('click', (e) => {
+function showMoreInfo(e) {
 
-        const currentElement = e.currentTarget;
+    if (e.target.tagName !== 'ARTICLE') {
+        return;
+    };
 
-        fetch(`${baseUrl}/jsonstore/cookbook/details/${recipe._id}`)
-            .then(res => res.json())
-            .then(recipe => {
-                
-                const mainArticle = renderDetais(recipe);
-                currentElement.innerHTML = mainArticle.innerHTML;
-            })
-            .catch(err => console.log(err));
-    });
+    const mainElement = document.querySelector('main');
+    mainElement.innerHTML = '';
 
-    return previewArticle;
+    fetch(`${detailsUrl}/${e.target._id}`)
+        .then(res => res.json())
+        .then(data => {
+            mainElement.appendChild(renderDetails(data));
+        })
+        .catch(err => console.log(err))
 }
 
 
+function renderDetails(details) {
 
-function div(props, ...data) {
-    return createElement('div', props, ...data);
-}
-
-function article(props, ...data) {
-    return createElement('article', props, ...data);
-}
-
-function img(props) {
-    return createElement('img', props);
-}
-
-function ul(props, ...data) {
-    return createElement('ul', props, ...data)
-}
-
-function li(...data) {
-    return createElement('li', {}, ...data)
-}
-function p(...data) {
-    return createElement('p', {}, ...data)
+    return article(
+        {},
+        heading(2, {}, details.name),
+        div(
+            { class: 'band' },
+            div(
+                { class: 'thumb' },
+                img({ src: details.img })
+            ),
+            div(
+                {class: 'ingredients'},
+                heading(3, {}, 'Ingredients:'),
+                listElement('ul', details.ingredients.length, {}, ...details.ingredients)
+            ),
+        ),
+        div(
+            {class: 'description'},
+            heading(3, {}, 'Preparation:'),
+            ...(details.steps.map(x => p({}, x)))
+        ),
+    );
 }
 
 function createElement(type, props, ...data) {
@@ -94,8 +97,11 @@ function createElement(type, props, ...data) {
         };
 
         element[prop] = props[prop];
-    }
+    };
 
+    if(data[0] === null) {
+        return element;
+    };
 
     for (let entry of data) {
 
@@ -104,45 +110,36 @@ function createElement(type, props, ...data) {
         };
 
         element.appendChild(entry);
-    }
+    };
 
     return element;
 }
 
-function renderDetais(recipe) {
-    const mainArticle = article(
-        {},
-        createElement('h2', {}, `${recipe.name}`),
-        div (
-            {class: 'band'},
-            div(
-                {class: 'thumb'},
-                img(
-                    {src: `${recipe.img}`}
-                ),
-            ),
 
-            div(
-                {class: 'ingredients'},
-                createElement('h3', {}, 'Ingredients:'),
-                ul(
-                    {},
-                    li(`${recipe.ingredients[0]}`),
-                    li(`${recipe.ingredients[1]}`),
-                    li(`${recipe.ingredients[2]}`),
-                    li(`${recipe.ingredients[3]}`),
-                ),
-            ),
-        ),
+function article(props, ...data) {
+    return createElement('article', props, ...data);
+}
+function div(props, ...data) {
+    return createElement('div', props, ...data);
+}
+function p(props, ...data) {
+    return createElement('p', props, ...data);
+}
+function heading(number, props, ...data) {
+    return createElement(`h${number}`, props, ...data);
+}
+function img(props, ...data) {
+    return createElement('img', props, ...data);
+}
 
-        div(
-            {class: 'description'},
-            createElement('h3', {}, 'Preparation:'),
-            p(`${recipe.steps[0]}`),
-            p(`${recipe.steps[1]}`),
-            p(`${recipe.steps[2]}`),
-        ),
-    );
+function listElement(type, numberOfLi, props, ...data) {
 
-    return mainArticle;
+    const listEl = createElement(type, props, null);
+
+    for (let i = 0; i < numberOfLi; i++) {
+        listEl.appendChild(createElement('li', {}, data[i]));
+    }
+
+    return listEl;
+
 }
